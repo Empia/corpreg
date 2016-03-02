df3fdee755a44ec0f15d7415d2c44f7c9f23090b
Stan <iamjacke@gmail.com>
2016-03-02 13:09:56 +0500

    C

---
+++ app/controllers/SignController.scala
@@ -0,0 +1,38 @@
+package controllers
+import play.api._
+import play.api.mvc._
+import play.twirl.api.Html
+
+import play.api.http.MimeTypes
+import play.api.libs.json._
+import play.api.cache._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formats
+import play.api.data.format.Formatter
+import play.api.data.FormError
+
+
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.{ Environment, LogoutEvent, Silhouette }
+import com.mohiva.play.silhouette.impl.authenticators.CookieAuthenticator
+import com.mohiva.play.silhouette.impl.providers.SocialProviderRegistry
+import forms._
+import models.User
+import play.api.i18n.MessagesApi
+
+import scala.concurrent.Future
+
+class SignController @Inject() (
+  val messagesApi: MessagesApi,
+  val env: Environment[User, CookieAuthenticator],
+  socialProviderRegistry: SocialProviderRegistry)
+  extends Silhouette[User, CookieAuthenticator] {	
+
+def index = SecuredAction.async { implicit request => 
+
+  Future.successful(Ok(views.html.passport(request.identity )))
+}
+
+}
