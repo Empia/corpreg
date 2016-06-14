43950f61bce1cb9c229db1dcd82ac7e7ea7e6cee
Stan <iamjacke@gmail.com>
2016-06-14 22:40:51 +0500

    Favicon

--- app/controllers/DutyController.scala
+++ app/controllers/DutyController.scala
@@ -289,6 +289,7 @@ DutyProcess.checkPaymentProcess(phone,
 	fillsDAO,
 	fillAttributesDAO).map { r => 
 	val reqOpt = (r.json \ "Result" \ "CheckURL").asOpt[String]
+	println("reqOpt: "+reqOpt)
 	reqOpt match {
 		case Some(r) => Ok(  r.replace("\\", "") )
 		case _ => Ok( "Bad condition" )
