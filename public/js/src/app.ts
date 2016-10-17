157c92cf4056fc6cc70d94442ecbaa6c78eacf07
Stan <iamjacke@gmail.com>
2016-10-17 23:57:03 +0200

    Upload first stage

--- public/js/src/app.ts
+++ public/js/src/app.ts
@@ -10,16 +10,26 @@ export class App {
   name:string;
   constructor() {
     this.name = 'Angular2'
-    this.selectedOkved = []
+    this.selectedOkved = [];
 
-	 this.class = {hidden: false};   
 
-	 this.class2 = {hidden: true};   
-	 this.class3 = {hidden: true};   
-	 this.class4 = {hidden: true};   
-	 this.class5 = {hidden: true};   
-	 this.class6 = {hidden: true};   
-	 this.class7 = {hidden: true};   
+	 this.class = {okveds: [{code: "01.1", title:"Тестовый оквэд 01.1" }], hidden: false};   
+
+	 this.class2 = {okveds: [{code: "01.2", title:"Тестовый оквэд 01.2" }], hidden: true};   
+	 this.class3 = {okveds: [{code: "01.3", title:"Тестовый оквэд 01.3" }], hidden: true};   
+	 this.class4 = {okveds: [{code: "01.4", title:"Тестовый оквэд 01.4" }], hidden: true};   
+	 this.class5 = {okveds: [{code: "01.5", title:"Тестовый оквэд 01.5" }], hidden: true};   
+	 this.class6 = {okveds: [{code: "01.6", title:"Тестовый оквэд 01.6" }], hidden: true};   
+	 this.class7 = {okveds: [{code: "01.7", title:"Тестовый оквэд 01.7" }], hidden: true};   
+
+this.addToSelected = function(okved) {
+	this.selectedOkved.push(okved);
+}
+this.removeFromSelected = function(okved) {
+	this.selectedOkved = this.selectedOkved.filter(function(el) {
+	    return el.code !== okved.code;
+	});
+}
 
 
   }
