8531a87766392725caa08c8be9730db6b515ef75
Stan <iamjacke@gmail.com>
2016-10-20 23:57:11 +0200

    Upload first stage

--- public/js/src/app.ts
+++ public/js/src/app.ts
@@ -13,7 +13,33 @@ export class App {
     this.selectedOkved = [];
 
 
-	 this.class = {okveds: [{code: "01.1", title:"Тестовый оквэд 01.1" }], hidden: false};   
+	 this.class = {okveds: [{code: "01.1", title:"Выращивание однолетних культур" }], hidden: false};   
+
+
+this.allOkveds = [
+{okveds: [{code: "01.1", title:"Выращивание однолетних культур" }], 
+childs: [{code: "01.11", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: false},
+{okveds:[{code: "01.2", title: "Выращивание многолетних культур"}],
+childs: [{code: "01.21", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: true},
+{okveds:[{code: "01.3", title: "Выращивание рассады"}],
+childs: [{code: "01.31", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: true},
+{okveds:[{code: "01.4", title: "Животноводство"}],
+childs: [{code: "01.41", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: true},
+{okveds:[{code: "01.5", title: "Смешанное сельское хозяйство"}],
+childs: [{code: "01.51", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: true},
+{okveds:[{code: "01.6", title: "Деятельность вспомогательная в области производства сельскохозяйственных культур и послеуборочной обработки сельхозпродукции"}],
+childs: [{code: "01.61", title: "01.11 код", childs: [{code: "01.11.11", title: "01.11.11"}], }], 
+hidden: true},
+{okveds:[{code: "01.7", title: "Охота, отлов и отстрел диких животных, включая предоставление услуг в этих областях"
+}],
+childs: [{code: "01.11", title: "01.11 код", childs: [{code: "01.11.11", title: "n nn n01.11.11"}], }], 
+hidden: true}
+];
 
 	 this.class2 = {okveds: [{code: "01.2", title:"Тестовый оквэд 01.2" }], hidden: true};   
 	 this.class3 = {okveds: [{code: "01.3", title:"Тестовый оквэд 01.3" }], hidden: true};   
@@ -25,6 +51,14 @@ export class App {
 this.addToSelected = function(okved) {
 	this.selectedOkved.push(okved);
 }
+
+
+this.addChildToSelected = function(okvedParent) {
+	this.selectedOkved.push(okvedParent.okveds[0]);
+}
+
+
+
 this.removeFromSelected = function(okved) {
 	this.selectedOkved = this.selectedOkved.filter(function(el) {
 	    return el.code !== okved.code;
