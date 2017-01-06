00fca8b2aa2edb6b617fdc349d342457d5728a3d
Stan <iamjacke@gmail.com>
2017-01-07 01:50:29 +0500

    Update

--- app/controllers/EgrulController.scala
+++ app/controllers/EgrulController.scala
@@ -65,7 +65,7 @@ def find() = Action.async { implicit request =>
   import play.api.libs.json.{JsNull,Json,JsString,JsValue}
 
 ws.url(
-  "https://xn--c1aubj.xn--80asehdb/%D0%B8%D0%BD%D1%82%D0%B5%D0%B3%D1%80%D0%B0%D1%86%D0%B8%D1%8F/%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D0%B8/7030/")
+  "https://ru.rus.company/%D0%B8%D0%BD%D1%82%D0%B5%D0%B3%D1%80%D0%B0%D1%86%D0%B8%D1%8F/%D0%BA%D0%BE%D0%BC%D0%BF%D0%B0%D0%BD%D0%B8%D0%B8/7030/")
 .get().map { response =>
       println(response.body)
       //address region url
